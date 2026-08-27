15e0be788c0743f62733211f7c3ad8f3c07793e6
MaxiNP19 <maxi2017p@gmail.com>
2026-08-27 15:39:05 -0300

    app.js: error middleware

--- src/app.js
+++ src/app.js
@@ -4,9 +4,20 @@ export const app = express();
 
 app.use(express.json());
 
+app.use()
+
 app.get('/', (req, res) => {
     res.status(200).json({
         status: 'success',
         message: 'API: sistema de turnos y reservas'
     })
 });
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: err.message }); // 👈 SIEMPRE 500
+});
+
+
+
+
+
