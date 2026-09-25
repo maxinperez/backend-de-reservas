7c5887294b54f0707be987bf341b81468460f79c
MaxiNP19 <maxi2017p@gmail.com>
2026-09-25 19:12:09 -0300

    feat: Mongo_uri env

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -2,17 +2,16 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
-const requiredEnvVars = ['PORT', 'NODE_ENV', 'SERVICES_DATA_PATH', 'BOOKINGS_DATA_PATH'];
+const requiredEnvVars = ['PORT', 'NODE_ENV', 'MONGO_URI'];
 // uso una lista por si despues debo agregar mas variables
 for (const varName of requiredEnvVars) {
   if (!process.env[varName]) {
     throw new Error(
-      `Falta la variable de entorno requerida: ${varName}. Revisá tu archivo .env`
+      `Falta configurar la variable de entorno requerida: ${varName}. Revisá tu archivo .env`
     );
   }
 }
 
 export const PORT = process.env.PORT;
-export const SERVICES_DATA_PATH = process.env.SERVICES_DATA_PATH;
 export const NODE_ENV = process.env.NODE_ENV;
-export const BOOKINGS_DATA_PATH = process.env.BOOKINGS_DATA_PATH;
+export const MONGO_URI = process.env.MONGO_URI;
